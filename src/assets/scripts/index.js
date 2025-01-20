6e72905f22e4f2648e9e28911c3bee73917311a0
meesooqa <meesooqa@hotmail.com>
2025-01-20 11:39:03 +0300

    add bootstrap 5.3.3

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,3 +1,1 @@
-import '../libs/jquery/jquery-3.5.1.slim.min';
-import '../libs/popper/2.9.2/popper.min';
-import '../libs/bootstrap/4.5.2/bootstrap.min';
+import '../libs/bootstrap/5.3.3/bootstrap.bundle.min';
