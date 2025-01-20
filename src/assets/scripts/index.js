ef0d450ba37c0c1ca1aaa7e84f85dfaf9a3e4cb9
meesooqa <meesooqa@hotmail.com>
2025-01-20 11:33:14 +0300

    add bootstrap 4.5.2

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,3 @@
+import '../libs/jquery/jquery-3.5.1.slim.min';
+import '../libs/popper/2.9.2/popper.min';
+import '../libs/bootstrap/4.5.2/bootstrap.min';
--- src/scripts/index.js
+++
@@ -1,1 +0,0 @@
-// import './modules/tmp.js';
